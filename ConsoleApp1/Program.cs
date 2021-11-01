612650f2899ecd18a0d4c8b83de05bb64e14ef2c
Anuj-Makkar <93370718+Anuj-Makkar@users.noreply.github.com>
2021-11-01 10:25:44 +0530

    update101

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello ");
-            
+            Console.Writeline("update");
             Console.WriteLine("Hello ");
             Console.WriteLine("Hello bro ");
 
